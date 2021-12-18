0ef587071e642310e7d294d6df8d885fc66b2d8f
deepali <deepaalipatil@gmail.com>
2021-12-18 17:28:51 +0530

    ADDED: main class for equalcompare method

---
+++ src/com/bridgelabz/linecomparision2.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+
+
+public class linecomparision2 extends  linecomparision{
+	public static void main(String[] args){
+		 linecomparision2 obj = new linecomparision2();
+		 obj.equalcompare();
+
+		 System.out.println("Welcome To Line Comparison Computation Program.\n");
+
+	      
+	      
+	
+	 }
+}
