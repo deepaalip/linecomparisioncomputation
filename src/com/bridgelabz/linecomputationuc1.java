994c0a2811615a9265edf93c98639207f487a6c1
deepaalip <95680526+deepaalip@users.noreply.github.com>
2021-12-27 18:43:51 +0530

    UPDATED:

--- src/com/bridgelabz/linecomputationuc1.java
+++ src/com/bridgelabz/linecomputationuc1.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 
 import java.util.Scanner;
 
-public class linecomputationuc1 {
+public class linecomputation {
 
 	public static void main(String[] args){
 	      System.out.println("Welcome To Line Comparison Computation Program.\n");
